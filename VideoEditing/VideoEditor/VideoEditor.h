8d272ac71b8f3772a6d112ba925c15240eae0926
flaviu88 <flaviu.silaghi@3pillarglobal.com>
2016-11-25 15:36:28 +0200

    create project

---
+++ VideoEditing/VideoEditor/VideoEditor.h
@@ -0,0 +1,19 @@
+//
+//  VideoEditor.h
+//  VideoEditor
+//
+//  Created by Flaviu Silaghi on 25/11/16.
+//  Copyright © 2016 3PillarGlobal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VideoEditor.
+FOUNDATION_EXPORT double VideoEditorVersionNumber;
+
+//! Project version string for VideoEditor.
+FOUNDATION_EXPORT const unsigned char VideoEditorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VideoEditor/PublicHeader.h>
+
+
